2a62367fefe57719fd6288e51e97143347e6a30f
15027051180 <1106965458@qq.com>
2018-07-24 10:45:33 +0800

    aa

--- src/test/java/com/kurumi/dispense/DumpTest.java
+++ src/test/java/com/kurumi/dispense/DumpTest.java
@@ -23,7 +23,7 @@ public class DumpTest {
 	
 	@Test
 	public void test1() {
-		
+		//aa bbb
 		String dirStr = "C:\\Users\\h2oco2\\Desktop\\ge1";
 		File dirFile = new File(dirStr);
 		File[] files = dirFile.listFiles();
